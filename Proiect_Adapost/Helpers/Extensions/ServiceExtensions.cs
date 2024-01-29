9b9c5551c00ccfee17145d312fd184921b82ccf8
RaduRalucag <ralucar2004@gmail.com>
2024-01-29 16:02:24 +0200

    initial push

---
+++ Proiect_Adapost/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Examen.Helpers.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //services.AddTransient<IRepository, Repository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //services.AddTransient<IService,Service>();
+            return services;
+        }
+    }
+}
---
+++ Proiect_Adapost/Helpers/MapperProfile.cs
@@ -0,0 +1,7 @@
+﻿using AutoMapper;
+namespace Proiect_Adapost.Helpers
+{
+    public class MapperProfile: Profile
+    {
+    }
+}
